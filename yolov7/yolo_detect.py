1a727c5ccfdd4dffb7be23983c2bb34eb43bf022
yangs1378 <87854369+yangs1378@users.noreply.github.com>
2022-08-19 13:22:33 +0800

    Add files via upload

---
+++ yolov7/yolo_detect.py
@@ -0,0 +1,96 @@
+import argparse
+import copy
+import time
+from pathlib import Path
+import numpy as np
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+from numpy import random
+
+from yolov7.models.experimental import attempt_load
+from yolov7.utils.datasets import LoadStreams, LoadImages, letterbox
+from yolov7.utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from yolov7.utils.plots import plot_one_box
+from yolov7.utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
+
+class YOLO_DET:
+    def __init__(self, weights='yolov7.pt'):
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self.weights = weights
+        self.imgsz = 640
+        self.trace = False
+        self.augment = False
+        self.conf_thres = 0.25
+        self.iou_thres = 0.45
+        self.stride = 32
+        self.classes = None
+        self.agnostic_nms = False
+        # Initialize
+        set_logging()
+        self.half = self.device.type != 'cpu'  # half precision only supported on CUDA
+        # Load model
+        self.model = attempt_load(self.weights, map_location=self.device)  # load FP32 model
+        stride = int(self.model.stride.max())  # model stride
+        img_sz = check_img_size(self.imgsz, s=stride)  # check img_size
+
+        if self.trace:
+            model = TracedModel(self.model, self.device, self.imgsz)
+
+        if self.half:
+            self.model.half()  # to FP16
+        # Run inference
+        if self.device.type != 'cpu':
+            self.model(torch.zeros(1, 3, img_sz, img_sz).to(self.device).type_as(next(self.model.parameters())))  # run once
+
+    def img_trans(self, image):
+        img = letterbox(image, self.imgsz, stride=self.stride)[0]
+
+        # Convert
+        img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
+        img = np.ascontiguousarray(img)
+        img = torch.from_numpy(img).to(self.device)
+        img = img.half() if self.half else img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
+        return img
+
+    def detect(self, image):
+        pred = self.model(image, augment=self.augment)[0]
+        # Apply NMS
+        pred = non_max_suppression(pred, self.conf_thres, self.iou_thres, classes=self.classes, agnostic=self.agnostic_nms)
+        return pred
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default='yolov7.pt', help='model.pt path(s)')
+    parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='display results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
+    parser.add_argument('--name', default='exp', help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    parser.add_argument('--no-trace', action='store_true', help='don`t trace model')
+    opt = parser.parse_args()
+    print(opt)
+    #check_requirements(exclude=('pycocotools', 'thop'))
+
+    with torch.no_grad():
+        if opt.update:  # update all models (to fix SourceChangeWarning)
+            for opt.weights in ['yolov7.pt']:
+                detect()
+                strip_optimizer(opt.weights)
+        else:
+            detect()
